6bcb17550f2a4096145ef6a49fcf76dce0016a40
Andrew Mueller <amdrwe@gmail.com>
2013-11-15 23:36:06 -0700

    Add extant files

---
+++ src/onionnpr/OnionNPR.java
@@ -0,0 +1,87 @@
+ /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onionnpr;
+import java.net.*;
+import java.util.Random;
+import java.util.Scanner;
+/**
+ *
+ * @author andy
+ */
+public class OnionNPR {
+
+    /**
+     * @param args the command line arguments
+     */
+    Scanner input = new Scanner(System.in);
+    Random r = new Random();
+        ScrapedPageOnion theOnion = new ScrapedPageOnion(new URL("http://theonion.com/"));
+        ScrapedFeedNPR npr = new ScrapedFeedNPR(new URL("http://www.npr.org/rss/rss.php?id=1001"));
+    int selection;
+    public static void main(String[] args) throws Exception{
+        // TODO code application logic here
+        OnionNPR onionnpr = new OnionNPR();
+    }
+    
+    public OnionNPR() throws Exception{
+
+        this.printIntroText();
+        selection = input.nextInt();
+            switch(selection)
+            {
+                case 1:         System.out.print("Choices are:\n1. The Onion\n2. NPR\n3. Quit\n"); this.playGame(); break;
+
+                default: System.out.println("Please make a valid selection");
+            }
+        
+    }
+    
+    private void printIntroText(){
+        System.out.println("The Onion, or NPR?");
+        System.out.println("Menu:");
+        System.out.println("1. Play");
+        System.out.println("2. Quit");
+        System.out.print(">");
+    }
+    
+    private void playGame(){
+
+        boolean isOnion;
+        int s = 0;
+        int randint = r.nextInt(100);
+        if (randint < 50){
+            isOnion = true;
+            theOnion.printRandomTitle();
+        }
+        else
+        {
+            isOnion = false;
+            npr.printRandomTitle();
+        }
+        System.out.println(">");
+        s = input.nextInt();
+        switch(s)
+        {
+            case 1: if (isOnion)
+            {
+                System.out.println("Correct");
+            }
+            else{ System.out.println("Wrong");
+            } playGame(); break;
+            case 2: if (!isOnion)
+            {
+                System.out.println("Correct");
+            }
+                else
+            {
+                System.out.println("Wrong");
+            } playGame(); break;
+            case 3: System.out.println("Thanks for playing!"); break;
+            default: System.out.println("Invalid selection"); playGame(); break;
+        }
+        
+        
+    }
+}
---
+++ src/onionnpr/ScrapedPageOnion.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onionnpr;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.*;
+/**
+ *
+ * @author andy
+ */
+public class ScrapedPageOnion {
+    private ArrayList<String> titles = new ArrayList<String>();
+    Random r = new Random();
+    public ScrapedPageOnion(URL url) throws Exception{
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(url.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null){
+            if (Pattern.compile(".*title=\".*\"").matcher(inputLine).matches()){
+                String[] titlesarray = inputLine.split("title=");
+                for (int i = 0; i < titlesarray.length; i++){
+                   titles.add(titlesarray[i].split(">")[0]);
+                }
+            }
+//            System.out.println(inputLine);
+        }
+        this.filterArray();
+        in.close();
+    }
+    
+    private void filterArray() {
+                    ArrayList<Integer> filternumbers = new ArrayList<Integer>();
+        for (int i = 0; i < titles.size(); i++){
+            String toFilter = titles.get(i);
+
+            if (Pattern.compile("\".*\"").matcher(toFilter).matches() == false)
+            {
+                filternumbers.add(i);
+            }
+            else if (toFilter.charAt(0) != '"'){
+                filternumbers.add(i);
+            }
+        }
+        for (int i = 0; i < filternumbers.size(); i++){
+            titles.set(filternumbers.get(i), null);
+        }
+        while (titles.contains(null)){
+            titles.remove(null);
+        }
+        titles.remove(0);
+        for (int i = 0; i < titles.size(); i++)
+        {
+            titles.set(i, titles.get(i).split("\"")[1]);
+        }
+    }
+    
+    public void printAllTitles() {
+        for (int i = 0; i < titles.size(); i++){
+            System.out.println("\"" + titles.get(i) + "\"\n");
+        }
+    }
+    
+        public void printRandomTitle() {
+        System.out.println(titles.get(r.nextInt(titles.size())));
+    }
+}
