cac266dae096744ef4d629f833252f2486983983
Andrew Mueller <amdrwe@gmail.com>
2013-11-15 23:50:46 -0700

    Remove some NetBeans default template garbage

--- src/onionnpr/ScrapedFeedNPR.java
+++ src/onionnpr/ScrapedFeedNPR.java
@@ -1,52 +1,51 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package onionnpr;
+
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.regex.*;
+
 /**
  *
  * @author andy
  */
 public class ScrapedFeedNPR {
+
     private ArrayList<String> titles = new ArrayList<String>();
     Random r = new Random();
-    public ScrapedFeedNPR(URL url) throws Exception{
+
+    public ScrapedFeedNPR(URL url) throws Exception {
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(url.openStream()));
         String inputLine;
-        while ((inputLine = in.readLine()) != null){
-            if (Pattern.compile(".*<title>.*").matcher(inputLine).matches()){
+        while ((inputLine = in.readLine()) != null) {
+            if (Pattern.compile(".*<title>.*").matcher(inputLine).matches()) {
                 titles.add(inputLine);
             }
-            //System.out.println(inputLine);
         }
         this.trimStrings();
         in.close();
     }
-    
+
     private void trimStrings() {
-        for (int i = 0; i < titles.size(); i++){
+        for (int i = 0; i < titles.size(); i++) {
             String toTrim = titles.get(i);
             titles.set(i, toTrim.substring(6, (toTrim.length() - 1 - 7)));
         }
         titles.remove(0);
-        for (int i = 0; i < titles.size(); i++){
+        for (int i = 0; i < titles.size(); i++) {
             titles.set(i, titles.get(i).split("<title>")[1]);
         }
         titles.remove(0);
     }
-    
+
     public void printAllTitles() {
-        for (int i = 0; i < titles.size(); i++){
+        for (int i = 0; i < titles.size(); i++) {
             System.out.println("\"" + titles.get(i) + "\"\n");
         }
     }
-    
+
     public void printRandomTitle() {
         System.out.println(titles.get(r.nextInt(titles.size())));
     }
